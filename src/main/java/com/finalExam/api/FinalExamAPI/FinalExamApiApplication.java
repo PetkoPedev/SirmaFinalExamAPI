4236232cd87bd4a263485e327a513ccfb81ed7e4
Petko Pedev <petko.pedev@gmail.com>
2024-09-07 11:34:41 +0300

    Initial commit of the backend api.

---
+++ src/main/java/com/finalExam/api/FinalExamAPI/FinalExamApiApplication.java
@@ -0,0 +1,13 @@
+package com.finalExam.api.FinalExamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalExamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalExamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalExam/api/FinalExamAPI/FinalExamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalExam.api.FinalExamAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalExamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
