beb62ccd4406930eb6cc1e3ba847e424f71aba47
Petko Pedev <petko.pedev@gmail.com>
2024-09-10 15:46:34 +0300

    Add TeamController and FootballMatchController

---
+++ src/main/java/com/finalExam/api/FinalExamAPI/controller/FootballMatchController.java
@@ -0,0 +1,61 @@
+package com.finalExam.api.FinalExamAPI.controller;
+
+import com.finalExam.api.FinalExamAPI.model.FootballMatch;
+import com.finalExam.api.FinalExamAPI.service.FootballMatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/matches")
+public class FootballMatchController {
+
+    @Autowired
+    private FootballMatchService footballMatchService;
+
+    @GetMapping
+    public List<FootballMatch> getAllMatches(){
+        return footballMatchService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FootballMatch> getMatchById(@PathVariable Long id){
+        try{
+            FootballMatch match = footballMatchService.findById(id);
+            return ResponseEntity.ok(match);
+        }
+        catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<FootballMatch> createMatch(@ModelAttribute FootballMatch match){
+        FootballMatch newMatch = footballMatchService.save(match);
+        return ResponseEntity.ok(newMatch);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<FootballMatch> updateMatch(@PathVariable Long id, @ModelAttribute FootballMatch match){
+        try{
+            FootballMatch existingMatch = footballMatchService.save(match);
+            return ResponseEntity.ok(existingMatch);
+        }
+        catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Void> deleteMatch(@PathVariable Long id){
+        try {
+            footballMatchService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/finalExam/api/FinalExamAPI/controller/TeamController.java
@@ -0,0 +1,61 @@
+package com.finalExam.api.FinalExamAPI.controller;
+
+import com.finalExam.api.FinalExamAPI.model.Team;
+import com.finalExam.api.FinalExamAPI.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teams")
+public class TeamController {
+
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping
+    public List<Team> getAllTeams(){
+        return teamService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Team> getTeamById(@PathVariable Long id){
+        try{
+            Team team = teamService.findById(id);
+            return ResponseEntity.ok(team);
+        }
+        catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Team> createTeam(@ModelAttribute Team team){
+        Team newTeam = teamService.save(team);
+        return ResponseEntity.ok(newTeam);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Team> updateTeam(@PathVariable Long id, @ModelAttribute Team team){
+        try{
+            Team existingTeam = teamService.save(team);
+            return ResponseEntity.ok(existingTeam);
+        }
+        catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @DeleteMapping("{/id}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable Long id){
+        try {
+            teamService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
